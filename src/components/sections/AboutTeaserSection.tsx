17e0a73d531429b1dd5c02ac16b63c3e122e4018
rebru-id <rebruid@gmail.com>
2026-04-03 15:37:04 +0800

    update css variables, cva & tailwind#2

--- src/components/sections/AboutTeaserSection.tsx
+++ src/components/sections/AboutTeaserSection.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import Button from "@/components/ui/Button.tsx";
+import Button from "@/components/ui/Button";
 
 export default function AboutTeaserSection() {
   return (
--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import Button from "@/components/ui/Button.tsx";
+import Button from "@/components/ui/Button";
 
 export default function HeroSection() {
   return (
